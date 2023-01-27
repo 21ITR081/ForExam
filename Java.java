8b377acdf5bc975ee15f9802fc8ea748f8fe8877
Pranesh Kangeyan <praneshkangeyanse.21it@kongu.edu>
2023-01-27 21:28:09 +0530

    Hello this is For Exam

---
+++ Java.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class hello
+{
+    public static void main(String[]args)
+    {
+    Scanner scan =new Scanner(System.in);
+    System.out.println("Enter the Value of A =");
+    int a=scan.nextInt();
+    System.out.println(a);
+    }
+}
